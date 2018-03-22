2f6d631ec9986e760146a536e077aff0b6147c7d
Andreas Möller <am@localheinz.com>
2018-03-22 12:00:02 +0100

    Fix: Add fixture

---
+++ test/Fixture/ClassesHaveTests/WithTests/Src/ExampleTest.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * Copyright (c) 2017 Andreas Möller.
+ *
+ * For the full copyright and license information, please view
+ * the LICENSE file that was distributed with this source code.
+ *
+ * @see https://github.com/localheinz/test-util
+ */
+
+namespace Localheinz\Test\Util\Test\Fixture\ClassesHaveTests\WithTests\Src;
+
+use PHPUnit\Framework;
+
+final class ExampleTest extends Framework\TestCase
+{
+}
