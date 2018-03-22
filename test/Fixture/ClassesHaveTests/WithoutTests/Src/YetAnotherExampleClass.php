be55f0c6aaa497cb0bc741f239956495465d8974
Andreas Möller <am@localheinz.com>
2018-03-22 08:51:30 +0100

    Enhancement: Assert that concrete class with private constructor needs test

---
+++ test/Fixture/ClassesHaveTests/WithoutTests/Src/YetAnotherExampleClass.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * Copyright (c) 2017 Andreas Möller.
+ *
+ * For the full copyright and license information, please view
+ * the LICENSE file that was distributed with this source code.
+ *
+ * @see https://github.com/localheinz/test-util
+ */
+
+namespace Localheinz\Test\Util\Test\Fixture\ClassesHaveTests\WithoutTests\Src;
+
+final class YetAnotherExampleClass
+{
+    private function __construct()
+    {
+    }
+}
