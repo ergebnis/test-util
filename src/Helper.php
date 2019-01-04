6fae8c158cae22f688823d5fa8e2b02af0c48a5a
Andreas Möller <am@localheinz.com>
2019-01-05 00:43:59 +0100

    Fix: Run 'make cs'

--- src/Helper.php
+++ src/Helper.php
@@ -3,7 +3,7 @@
 declare(strict_types=1);
 
 /**
- * Copyright (c) 2017 Andreas Möller.
+ * Copyright (c) 2017 Andreas Möller
  *
  * For the full copyright and license information, please view
  * the LICENSE file that was distributed with this source code.
@@ -196,7 +196,7 @@ trait Helper
             throw Exception\NonExistentDirectory::fromDirectory($directory);
         }
 
-        \array_walk($excludeClassyNames, static function ($excludeClassyName) {
+        \array_walk($excludeClassyNames, static function ($excludeClassyName): void {
             if (!\is_string($excludeClassyName)) {
                 throw Exception\InvalidExcludeClassName::fromClassName($excludeClassyName);
             }
